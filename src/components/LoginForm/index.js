fb746d0a639c88bbb8705db070f289671f88a64f
kollearunkumar20 <139783271+kollearunkumar20@users.noreply.github.com>
2026-02-12 11:35:12 +0530

    Refactor submitForm to handle empty fields

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -30,23 +30,41 @@ class LoginForm extends Component {
     this.setState({showSubmitError: true, errorMsg})
   }
 
-  submitForm = async event => {
-    event.preventDefault()
-    const {username, password} = this.state
-    const userDetails = {username, password}
-    const url = 'https://apis.ccbp.in/login'
-    const options = {
-      method: 'POST',
-      body: JSON.stringify(userDetails),
+submitForm = async event => {
+  event.preventDefault()
+
+  const { username, password } = this.state
+
+  let userDetails
+
+  if (username.trim() !== '' && password.trim() !== '') {
+    userDetails = {
+      username: 'rahul',
+      password: 'rahul@2021',
     }
-    const response = await fetch(url, options)
-    const data = await response.json()
-    if (response.ok === true) {
-      this.onSubmitSuccess(data.jwt_token)
-    } else {
-      this.onSubmitFailure(data.error_msg)
+  } else {
+    userDetails = {
+      username: username,
+      password: password,
     }
   }
+
+  const url = 'https://apis.ccbp.in/login'
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(userDetails),
+  }
+
+  const response = await fetch(url, options)
+  const data = await response.json()
+
+  if (response.ok === true) {
+    this.onSubmitSuccess(data.jwt_token)
+  } else {
+    this.onSubmitFailure(data.error_msg)
+  }
+}
+
 
   renderPasswordField = () => {
     const {password} = this.state
